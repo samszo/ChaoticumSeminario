b8f18f648c136e1f32b38c8cad38b93c99fd469d
Samuel Szoniecky <samszon@gmail.com>
2022-10-17 09:50:44 +0200

    01DEsign

---
+++ asset/js/sourcesInfos.js
@@ -0,0 +1,18 @@
+[{
+    "id":"btnConf",
+    "label":"Conf. errance",
+    "fnct":"aleaDia"
+}, {
+    "id": "btnCita",
+    "label": "Si t'as scion",
+    "url": "http://localhost/samszo/omk/api/items?resource_class_id=73&sort_by=random&limit=1&offset=1&per_page=1"
+}, {
+    "id": "btnDia",
+    "label": "Doctogramme",
+    "url": "http://localhost/samszo/omk/api/media?sort_by=random&limit=1&offset=1&per_page=1"
+}, {
+    "id": "btnLoc",
+    "label": "In terre locuteur",
+    "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vRv3Z99vpH6N8OFZ_VA1ft8YrBMD5cIYTL5ME0ZmxXmMCKcx0coreLZdha1QvX4i2PrYkWeX0PfXc0h/pub?gid=316802798&single=true&output=csv",
+    "fnct":"setLoc"
+}]
--- getConfsInfos.php
+++ getConfsInfos.php
@@ -2,6 +2,8 @@
 //récupère la liste des conférences
 $directory = '/var/www/html/samszo/conf_errance/';//getcwd();
 $webPath = "https://samszo.univ-paris8.fr/conf_errance/";
+$directory = '/Users/samszo/Sites/samszo/conf_errance/';
+$webPath = "http://localhost/samszo/conf_errance/";
 $scanned_directory = array_diff(scandir($directory), array('..', '.'));
 $confs = array();
 foreach ($scanned_directory as $c) {
@@ -11,22 +13,44 @@ foreach ($scanned_directory as $c) {
         $c=array(
         'urlSVG' => $webPath.$c."/".pathinfo($svgPath)['filename'].".svg",
         'urlSlide' => $webPath.$c."/slide.html",
+        'urlConf' => $webPath.$c."/index.html",
         'diapos' =>[]
         );
+        $c['infos']=getConfInfos($c['urlConf']);        
         $xml = simplexml_load_file($c['urlSVG']);
-        $xml->registerXPathNamespace('svg', 'http://www.w3.org/2000/svg');
-        //récupère les diapos
-        $result = $xml->xpath('//svg:rect');
-        foreach ($result as $node) {
-            $id = $node->attributes()['id']->__toString();
-            if(substr($id,0,6)=='slide_'){
-                $c['diapos'][]=$id;
-            }        
-        }
-        if(count($c['diapos'])){
-            $confs[]=$c;
+        if($xml){
+            $xml->registerXPathNamespace('svg', 'http://www.w3.org/2000/svg');
+            //récupère les diapos
+            $result = $xml->xpath('//svg:rect');
+            foreach ($result as $node) {
+                $id = $node->attributes()['id']->__toString();
+                if(substr($id,0,6)=='slide_'){
+                    $c['diapos'][]=$id;
+                }        
+            }
+            if(count($c['diapos'])){
+                $confs[]=$c;
+            }    
         }
     }
 }
 echo json_encode($confs);
+
+function file_get_contents_curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+function getConfInfos($url){
+    $html = file_get_contents_curl($url);
+    preg_match('/<title>(.+)<\/title>/',$html,$matches);
+    $infos['title'] = $matches[1];
+    return $infos;     
+}
+
 ?>
