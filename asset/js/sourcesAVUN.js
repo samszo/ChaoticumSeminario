0e9fc264ee675bf74dbf167b166cc84d754c8bdb
Samuel Szoniecky <samszon@gmail.com>
2022-10-20 12:40:36 +0200

    avun v 0

---
+++ asset/js/sourcesAVUN.js
@@ -0,0 +1,19 @@
+[
+    {
+           "id":"btnConf",
+       "label":"Conf. errance",
+       "fnct":"aleaDia"
+   }, {
+       "id": "btnCita",
+       "label": "Si t'as scion",
+       "url": "https://samszo.univ-paris8.fr/omk/api/items?resource_class_id=73&sort_by=random&limit=1&offset=1&per_page=1"
+   }, {
+       "id": "btnDia",
+       "label": "Doctogramme",
+       "url": "https://samszo.univ-paris8.fr/omk/api/media?sort_by=random&limit=1&offset=1&per_page=1&media_type=image/png"
+   }, {
+       "id": "btnLoc",
+       "label": "In terre locuteur",
+       "url": "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGdMEj_yvU1nziiYa6OFwBr_uRaGiznQihQgN5slQHBOR8iRo4LoRFd5-GeiQI3MxPQFpSyaXNGUly/pub?gid=831034866&single=true&output=csv",
+       "fnct":"setLoc"
+   }]
