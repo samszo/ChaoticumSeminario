0fabbcc170eb45f825713e049a4760935fe6f976
samszo <samszon@gmail.com>
2023-03-20 16:18:29 +0100

    genstory

---
+++ asset/js/sourcesGenStroy2023.js
@@ -0,0 +1,12 @@
+[{
+    "id": "btnGenStory2023",
+    "label": "P8 - ArTec - RIT 2023",
+    "url": "https://www.rit.motsvoir.org/api/items?resource_class_id=108",
+    "fnct":"genStory"
+}, {
+    "id": "btnGenStory2022",
+    "label": "P8 - ArTec - RIT 2022",
+    "url": "https://genstory.jardindesconnaissances.fr/api/items?resource_class_id=111",
+    "fnct":"genStory"
+}
+]
